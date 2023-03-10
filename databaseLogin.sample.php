89e370283b63102d139a1a80181b3f18f9ba68d1
cy0802 <amyyoyo0802@gmail.com>
2023-03-10 19:59:41 +0800

    add sample databaseLogin.php

---
+++ databaseLogin.sample.php
@@ -0,0 +1,15 @@
+<?php
+echo "<head><meta name='robots' content='noindex'></head>";
+$hostname = "";
+$username = "";
+$password = "";
+$database = "";
+
+$acc = "";
+$pwd = "";
+
+$mailerliteAPIToken = "";
+$verifyConst = [0, 0, 0, 0, 0];
+
+$emailjsToken = "";
+?>
