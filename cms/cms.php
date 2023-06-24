87747813bc1ede9a0ee05e83263cb4bff2d9b73b
Today-Asked <mao930710@gmail.com>
2023-06-24 12:43:34 +0800

    add RWD to CMS

--- cms/cms.php
+++ cms/cms.php
@@ -3,13 +3,63 @@ require_once "auth.php";
 ?>
 <!DOCTYPE HTML5>
 <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>後台索引頁</title>
     <meta name="robots" content="noindex">
+    <style>
+        .content{
+            width: 100%;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .list-group{
+            width: 80%;
+            max-width: 600px;
+        }
+        img{
+            height: 100%;
+        }
+        .list-group-item{
+            display: flex;
+            flex-wrap: wrap;
+            align-items: baseline;
+        }
+    </style>
 </head>
 <body>
-    <a href="./reviewComment.php">審核評論</a><br>
-    <a href="./reviewMsg.php">審核留言</a><br>
-    <a href='./alterComment.php'>刪除資料（評論 or 書籍）</a><br>
-    <a href="./picture.php">更新封面圖片</a><br>
-    <a href="./recalculateData.php">重新計算 book 資料表（按下此連結就會執行！）</a><br>
+    <div class="content">
+        <div class="list-group">
+            <a href="./reviewComment.php" type="button" class="list-group-item list-group-item-action" aria-current="true">
+                <div>
+                    <img src="star.png">審核評論
+                </div>
+            </a>
+            <a href="./reviewMsg.php" type="button" class="list-group-item list-group-item-action" aria-current="true">
+                <div>
+                    <img src="comment.png"> 審核留言
+                </div>
+            </a>
+            <a href="./alterComment.php" type="button" class="list-group-item list-group-item-action" aria-current="true">
+                <div>
+                    <img src="bin.png"> 刪除資料（評論 or 書籍）
+                </div>
+            </a>
+            <a href="./picture.php" type="button" class="list-group-item list-group-item-action" aria-current="true">
+                <div>
+                    <img src="upload.png"> 更新封面圖片
+                </div>
+            </a>
+            <a href="./recalculateData.php" type="button" class="list-group-item list-group-item-action" aria-current="true">
+                <div>
+                    <img src="calculator.png"> 
+                    重新計算 book 資料表
+                </div>
+                <div>（按下此連結就會執行！）</div>
+            </a>
+        </div>
+    </div>
 </body>
--- loginCMS.php
+++ loginCMS.php
@@ -19,35 +19,44 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>後台登入</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
+        .content{
+            width: 100%;
+            height: 100%;
+            display:flex;
+            align-items: center;
+            justify-content: center;
+        }
         #login{
-            position: absolute;
-            top: 40%;
-            left: 50%;
-            transform: translateY(-40%);
-            transform: translateX(-50%);
+            width: 80%;
+            max-width: 450px;
             border-width: 2px;
             border-color: rgb(200, 200, 200);
             border-style: solid; 
             border-radius: 5px;
-            padding: 2%;
+            padding-left: 5%;
+            padding-right: 5%;
+            padding-top: 2%;
         }
     </style>
 </head>
 <body>
     <center> 
-        <div id="login">
-            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-                <div class="form-group">
-                    <label for="exampleInputEmail1">帳號：</label>
-                    <input name = "account" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">密碼：</label>
-                    <input name="password" type="password" class="form-control" id="exampleInputPassword1">
-                </div>
-                <button type="submit" class="btn btn-secondary" style="margin: 2%">送出</button>
-            </form>
+        <div class="content">
+            <div id="login" class="card">
+                <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">帳號：</label>
+                        <input name = "account" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">密碼：</label>
+                        <input name="password" type="password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <button type="submit" class="btn btn-secondary" style="margin: 2%">送出</button>
+                </form>
+            </div>
         </div>
     </center>
 </body>
