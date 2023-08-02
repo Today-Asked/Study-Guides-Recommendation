3a5dfcd5554fba36dacc2d5daa34a391176c8c47
Today-Asked <mao930710@gmail.com>
2023-08-03 00:54:34 +0800

    GA in new domain

--- message_board.php
+++ message_board.php
@@ -11,6 +11,15 @@
         gtag('js', new Date());
         gtag('config', 'G-ZXCEF0Q5KK');
     </script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-35LSXM0QBG"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-35LSXM0QBG');
+    </script>
     <script>
         $("document").ready(function(){
             loadData(0);
